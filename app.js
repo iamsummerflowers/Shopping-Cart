b35e70232f855df091450e0008f35d325211b321
sum_flowers <summer.flowers94@gmail.com>
2021-08-17 19:57:25 -0500

    Updated dynamic product display

--- app.js
+++ app.js
@@ -1,56 +1,55 @@
-//1. Establish variables
-const slides = document.querySelectorAll(".slide");
-const nextBtn = document.querySelector(".nextBtn");
-const prevBtn = document.querySelector(".prevBtn");
+//1. variables
+const cartBtn = document.querySelector(".cartBtn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItem = document.querySelector(".close-item");
+const cartTotal = document.querySelector(".close-total");
+const cartContent = document.querySelector(".cart-content");
+const productDOM = document.querySelector(".products-center");
 
-//2. Index Slides
-slides.forEach(function(slide,index){
-    slide.style.left = `${index * 100}%`;
-});
+// 2. create cart item
+let cart = [];
 
-//3. Set Counter Variable and setup click activation
-let counter = 0;
-
-nextBtn.addEventListener('click',function(){
-    counter++;
-    //5.invoke carousel
-    carousel();
-
-});
-prevBtn.addEventListener('click',function(){
-    counter--;
-    //5.invoke carousel
-    carousel();
-});
-
-//4. Create carousel functionality
-function carousel(){
-    //6A. working with slides
-    if(counter===slides.length){
-        counter=0;
+//3. responsible for getting the products first locally and then from json //setup method
+class Products{
+    async getProducts(){
+        try {
+            let result = await fetch("product.json");
+            let data = await result.json();
+            let products = data.items;
+            products = products.map(item => {
+                const {title,price} = item.fields;
+                const {id} = item.sys //Question: why no semi-colon?
+                const image = item.fields.image.fields.file.url;
+                return {title,price,id,image} //Question: why no semi-colon?
+            })
+            return products;
+        } catch (error) {
+            console.log(error);
+        }
     }
-    if(counter < 0){
-        counter=slides.length-1;
-    }
-
-// //6B.continued
-// if(counter < slides.length-1){
-//     nextBtn.style.display = "block"
-// }
-// else{
-//     nextBtn.style.display="none";
-// }
-// if(counter > 0){
-//     prevBtn.style.display="block";
-// }
-// else{
-//     prevBtn.style.display="none";
-// }
-//4. continued
-    slides.forEach(function(slide){
-        slide.style.transform = `translateX(-${counter * 100}%)`
-    })
 }
 
-// //6B. Or hide prev button
-// prevBtn.style.display = "none";
+//4. display products - responsible for getting items returned from product and displaying them, maniulating them, or retrieving them //ads data dynamically (comment out single item in html)
+class UI {
+ displayProducts(products){
+    console.log(products);
+
+ }
+}
+
+//5. local storage class that deals with local storage
+class Storage {
+
+}
+
+//6. call functions in 3 and 4 here
+document.addEventListener("DOMContentLoaded",()=>{
+const ui = new UI();
+const products = new Products();
+
+// 7. get all products go back to 3 and grab data from json
+products.getProducts().then(products => ui.displayProducts(products));
+});
